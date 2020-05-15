36c1c3c68e6694a0ad9fed9b7381596537d113a6
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-15 19:59:38 -0300

    O programa a seguir cria duas variáveis chamadas var1 e var2

---
+++ src/Example2.java
@@ -0,0 +1,16 @@
+/* O programa a seguir cria duas variáveis chamadas var1 e var2. */
+
+public class Example2 {
+    public static void main(String[] args) {
+        /* Instruções de declaração de variaveis interio */
+        int var1;
+        int var2;
+        var1 = 1024; /* Esta atribuindo um valor de 1024 a var1 */
+
+        System.out.println("Valor constande de Var1 é: " + var1);
+
+        var2 = var1 / 2;
+        System.out.println("Var2 contem var1 / 2: ");
+        System.out.println(var2);
+    }
+}
