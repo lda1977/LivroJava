144c89bf3b04bcc1af9fca326d17ef4816adb535
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-15 18:30:54 -0300

    Primeiro Programa do livro.

---
+++ src/Example.java
@@ -0,0 +1,13 @@
+/**
+ *  a classe é a unidade básica de
+ *  Encapsulamento onde Example é o nome da classe.
+ */
+public class Example {
+
+    /* metodos ou membros da classe */
+    /* Um programa java começa com um chamada a main */
+    /* Essa linha começa o método main( ) */
+    public static void main(String[] args){
+        System.out.println("Java drives de Web: ");
+    }
+}
