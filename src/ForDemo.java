0e4d1ec85118eac940a80f119cd3406c95aac047
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-15 22:17:42 -0300

    Se o resultado desse teste for verdadeiro, o laço for continuará a iterar. Se for falso, o laço será encerrado.

---
+++ src/ForDemo.java
@@ -0,0 +1,15 @@
+/**
+ * Demonstra o laço for.
+ * Se o resultado desse teste for verdadeiro, o laço
+ * for continuará a iterar. Se for falso, o laço será encerrado.
+ */
+public class ForDemo {
+    public static void main(String[] args) {
+        int contador;
+
+        for (contador = 0; contador < 5; contador++) {
+            System.out.println("O contador: " + contador);
+        }/* Fim For */
+        System.out.println("Demo!!!!");
+    }
+}
