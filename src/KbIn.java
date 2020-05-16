254262f29106d7c83801da9384fc93e1590a2b9b
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-16 15:09:37 -0300

    Demonstra a inicialização dinâmica.

---
+++ src/KbIn.java
@@ -0,0 +1,8 @@
+public class KbIn {
+    public static void main(String[] args) throws java.io.IOException {
+        char ch;
+        System.out.println("Enter ");
+        ch = (char) System.in.read();
+        System.out.println(ch);
+    }
+}
