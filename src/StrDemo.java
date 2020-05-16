e43671b2acfe9d1291fe76eb6aba1f17179b428b
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-16 14:05:31 -0300

    Ele usa as sequências de escape \n e \t.

---
+++ src/StrDemo.java
@@ -0,0 +1,10 @@
+/**
+ * Ele usa as sequências de escape \n e \t.
+ * */
+public class StrDemo {
+    public static void main(String[] args){
+        System.out.println("Primeira Linha \nSegunda Linha");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
