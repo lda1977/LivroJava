a07bc2af429b6e655b40d0bfd99d3aaad8cf498f
Eng. Luciano - CREA-PR-103589-D <lucianoa2028@gmail.com>
2020-05-15 22:34:33 -0300

    Demonstra um bloco de código

---
+++ src/BlockDemo.java
@@ -0,0 +1,17 @@
+/**
+ * Demonstra um bloco de código.
+ */
+public class BlockDemo {
+    public static void main() {
+        double i, j, d;
+        i = 5;
+        j = 10;
+
+        if (i != 0) {
+            System.out.println("I não é igual a zero: ");
+            d = j / i;
+            System.out.println("J / i é : " + d);
+        }/* Fim IF */
+    }
+
+}
